616ee9946a6c29e96dbe3b2089413b5ee7def350
Adam La Rosa <adam.larosa@flatironschool.com>
2022-03-19 20:46:47 -0700

    movies#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,9 @@ class MoviesController < ApplicationController
 		render json: Movie.all
 	end
 
+	def show
+		render json: Movie.find( params[:id] )
+	rescue ActiveRecord::RecordNotFound
+  		render json: "Bird not found", status: :not_found
+	end
 end
