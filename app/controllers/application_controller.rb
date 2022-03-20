e8464f446a8d53d306d7d23447b9cce8fcca147a
Adam La Rosa <adam.larosa@flatironschool.com>
2022-03-20 12:00:40 -0700

    Error handling & User validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    rescue_from ActiveRecord::RecordInvalid, with: :not_valid
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
     before_action :authorize
 
     def current_user
-        false
+        User.find_by(id: session[:user])
     end
 
     private
+
+    def not_found(invalid)
+        render json: { error: "#{invalid.model} not found" }
+    end
+
+    def not_valid(invalid)
+        render json: { errors: invalid.record.errors.full_messages }
+    end
 
     def logged_in?
         !!current_user
