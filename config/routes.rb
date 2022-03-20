405ae1bd0076574ff2f0e2c2d2f964222d903a6c
Adam La Rosa <adam.larosa@flatironschool.com>
2022-03-19 21:22:16 -0700

    Restricted routes in movies resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-	resources :movies
+	resources :movies, only: [:index, :show]
 	get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
