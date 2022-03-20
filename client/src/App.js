4ab4196e22e2bd02c1a7fb88927aafd2eb1bc0f0
Adam La Rosa <adam.larosa@flatironschool.com>
2022-03-19 23:05:18 -0700

    fetch attempt

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
+  const sendAGET = () => {
+    fetch("/movies").then( r => r.json() ).then( console.log )
+  }
   return (
     <div className="App">
       <header className="App-header">
        <h1>LIT HARBOR</h1>
+       <button onClick={sendAGET}>X</button>
       </header>
     </div>
   );
