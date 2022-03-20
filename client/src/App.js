4971b4119d282654189c0d81033f285193b12da5
Adam La Rosa <adam.larosa@flatironschool.com>
2022-03-20 12:54:00 -0700

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import './stylesheets/App.css';
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
+function Main() {
   const sendAGET = () => {
     fetch("/movies").then( r => r.json() ).then( console.log )
   }
   return (
     <div className="App">
       <header className="App-header">
-       <h1>LIT HARBOR</h1>
-       <button onClick={sendAGET}>X</button>
+        <h1>LIT HARBOR</h1>
+        <button onClick={sendAGET}>X</button>
       </header>
     </div>
+  )
+}
+function App() {
+
+  return (
+    
+    <Routes>
+      <Route path="*" element={<Main />} />
+    </Routes>
   );
 }
 
