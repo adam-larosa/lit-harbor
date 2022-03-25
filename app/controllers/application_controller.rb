5f4d5cdcebc2aa2d85282c0596e3047bde4a9620
Adam La Rosa <adam.larosa@flatironschool.com>
2022-03-25 09:52:37 -0700

    style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,7 @@ class ApplicationController < ActionController::API
     before_action :authorize
 
     def current_user
-        true
-        #User.find_by( id: session[:user_id] )
+        User.find_by( id: session[:user_id] )
     end
 
     private
